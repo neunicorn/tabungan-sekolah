98fd2e0694dfd02ba066aca8b4ccc12a23c2894a
Muhamad Zulfan <73164719+neunicorn@users.noreply.github.com>
2021-12-29 11:53:14 +0700

    membuat project tabungan untuk project sbd uas

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>nabungyuk!</title>
+</head>
+<body>
+    <main>
+    
+            <div class="container">
+                <h2>Log in</h2>
+                <form action="login.php" method="POST">
+                    <div class = "login-space">
+                        <label for="username">username</label>
+                        <br>
+                        <img src="img/user2.png" alt="">
+                        <input type="text" name="username" placeholder="type your username">
+                    </div>
+                    <div class="login-space">
+                        <label for="password">password</label>
+                        <br>
+                        <img src="img/unlock.png" alt="">
+                        <input type="password" name="password" id=""> 
+                    </div>
+                    <div class="login-space">
+                        <button type="submit">LOG IN</button>
+                    </div>
+                </form>
+            </div>
+        
+        
+    </main>
+    <footer>
+
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
